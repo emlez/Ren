5c40ae9bd34df5686a3de0ed47bf301d808d3c72
EmlSn <eml.snzrmz@gmail.com>
2021-10-18 01:08:50 -0500

    Added map and modal (not working yet).

--- index.js
+++ index.js
@@ -29,12 +29,35 @@ function showSlides(n, no) {
   }
   for (i = 0; i < slide.length; i++) {
     slide[i].style.display = "none";
+    // slide[i].className = slide[i].className.replace("myImg", "");
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
     demo[i].className = demo[i].className.replace(" active", "");
   }
   slide[slideIndex[no] - 1].style.display = "block";
+  // slide[slideIndex[no] - 1].className += " myImg";
   dots[slideIndex[no] - 1].className += " active";
   demo[slideIndex[no] - 1].className += " active";
 }
+
+// // Modal
+// let modal = document.getElementById("myModal");
+
+// // Get the image and insert it inside the modal - use its "alt" text as a caption
+// let img = document.getElementsByClassName("myImg");
+// let modalImg = document.getElementById("img01");
+// let captionText = document.getElementById("caption");
+// img.onclick = function () {
+//   modal.style.display = "block";
+//   modalImg.src = this.src;
+//   captionText.innerHTML = this.alt;
+// };
+
+// // Get the <span> element that closes the modal
+// let i = document.getElementsByClassName("close")[0];
+
+// // When the user clicks on <span> (x), close the modal
+// i.onclick = function () {
+//   modal.style.display = "none";
+// };
